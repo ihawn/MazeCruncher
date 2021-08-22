cd029751b551c50ea2c1d8e4282d5410a54f2a48
ihawn <ihawn.pl15@gmail.com>
2021-08-22 09:26:06 -0500

    -

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::time::Instant;
 use std::io;
+use std::io::{stdin, stdout, Read, Write};
 mod generate;
 mod toimage;
 mod solve;
@@ -17,54 +18,21 @@ fn main()
     let mut anim_speed = 2;
 
 
-    println!("Use default settings?");
-    println!("y/n: ");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read");
-    let mut trimmed: String = input.trim().to_string();
+    let use_default = read_bool("Use default settings?\ny/n: ".to_string());
 
-    if trimmed == "n"
+    if !use_default
     {
-        println!();
-        println!("Enter a maze size: ");
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read");
-        let mut trimmed: String = input.trim().to_string();
-        match input.trim().parse::<u32>() {
-            Ok(i) => size = i as usize,
-            Err(..) => println!("Invalid Input: {}", trimmed),
-        };
-
+        size = read_int("Enter a maze size: ".to_string(), size);
         println!("A solution image will be saved to this directory");
 
         if size <= 2048
         {
-            println!("Do you want to display the solution animation? (takes longer but looks cool)");
-            println!("y/n: ");
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).expect("Failed to read");
-            let mut trimmed = input.trim().to_lowercase();
-            show_animation = trimmed == "y";
+            show_animation = read_bool("Do you want to display the solution animation? (takes longer but looks cool)\ny/s".to_string());
 
             if show_animation
             {
-                println!("Enter animation scale: ");
-                let mut input = String::new();
-                io::stdin().read_line(&mut input).expect("Failed to read");
-                trimmed = input.trim().to_string();
-                match trimmed.parse::<u32>() {
-                    Ok(i) => anim_scale = i as usize,
-                    Err(..) => println!("Invalid Input: {}", trimmed),
-                };
-
-                println!("Enter animation speed: ");
-                let mut input = String::new();
-                io::stdin().read_line(&mut input).expect("Failed to read");
-                trimmed = input.trim().to_string();
-                match trimmed.parse::<u32>() {
-                    Ok(i) => anim_speed = i as usize,
-                    Err(..) => println!("Invalid Input: {}", trimmed),
-                };
+                anim_scale = read_int("Enter animation scale: ".to_string(), anim_scale);
+                anim_speed = read_int("Enter animation speed: ".to_string(), anim_speed);
             }
         }
         else 
@@ -82,6 +50,41 @@ fn main()
 
     println!("Done!");
     println!("Solved and unsolved images of this maze saved to this directory");
-    println!("Press enter to continue");
-    io::stdin().read_line(&mut input);
+    pause();
+}
+
+fn read_int(message: String, default_value: usize) -> usize
+{
+    
+    let mut input = String::new();
+    println!();
+    println!("{}", message);
+
+    io::stdin().read_line(&mut input).expect("Failed to read");
+    let trimmed = input.trim().to_string();
+    match trimmed.parse::<u32>() {
+        Ok(i) => return i as usize,
+        Err(..) => println!("Invalid Input: {}", trimmed),
+    };
+
+    return default_value;
+}
+
+fn read_bool(message: String) -> bool
+{
+    let mut input = String::new();
+    println!();
+    println!("{}", message);
+
+    io::stdin().read_line(&mut input).expect("Failed to read");
+    let mut trimmed: String = input.trim().to_string().to_lowercase();
+    return trimmed == "y";
+}
+
+fn pause()
+{
+    let mut stdout = stdout();
+    stdout.write(b"Press Enter to continue...").unwrap();
+    stdout.flush().unwrap();
+    stdin().read(&mut [0]).unwrap();
 }
