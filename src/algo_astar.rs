5a00e1bcb3af19a1966f38000e7c2314e65e64fa
ihawn <ihawn.pl15@gmail.com>
2021-10-18 18:43:17 -0500

    Improvement to graph building algo

--- src/algo_astar.rs
+++ src/algo_astar.rs
@@ -110,24 +110,25 @@ fn graph_init(mtx: &[Vec<u8>], size: usize, start_x: usize, start_y: usize, end_
     let mut nodes: Vec<Node> = vec!();  
     let mut node_count: usize = 0;
 
+    let mut cur_node= Node{ node: MazeNode{
+        x: start_x,
+        y: start_y,
+        open: false,
+        closed: false,
+        g: usize::MAX,
+        f: 0,
+        index: node_count,
+        parent_index: 0
+    }, connected: vec!(), edge_weights: vec!() };
+
     //Add first node
-    nodes.push(Node{ node: MazeNode{
-            x: start_x,
-            y: start_y,
-            open: false,
-            closed: false,
-            g: usize::MAX,
-            f: 0,
-            index: node_count,
-            parent_index: 0
-        }, connected: vec!(), edge_weights: vec!() });
+    nodes.push(cur_node);
 
-    let mut matrix: Vec<Vec<u8>> = mtx.to_vec();
-
-    //Build the nodes
-    for i in 0..size
+    //Build the nodes and attach
+    let mut left_nodes: Vec<usize> = vec![usize::MAX; size]; //Container to store the last node to the left of current with a given y value
+    for i in 1..size-1
     {
-        for j in 0..size
+        for j in 1..size-1
         {         
             //Conditions to be a node: Either dead end, 3-way intersection, or 4-way intersection
             if mtx[i][j] != u8::MAX && 
@@ -136,8 +137,7 @@ fn graph_init(mtx: &[Vec<u8>], size: usize, start_x: usize, start_y: usize, end_
             (mtx[i+1][j] != u8::MAX && mtx[i-1][j] != u8::MAX && mtx[i][j+1] != u8::MAX && mtx[i][j-1] != u8::MAX))
             {
                 node_count += 1;
-                
-                nodes.push(Node{ node: MazeNode{
+                cur_node = Node{ node: MazeNode{
                     x: i,
                     y: j,
                     open: false,
@@ -146,9 +146,12 @@ fn graph_init(mtx: &[Vec<u8>], size: usize, start_x: usize, start_y: usize, end_
                     f: 0,
                     index: node_count,
                     parent_index: 0
-                }, connected: vec!(), edge_weights: vec!(), });
+                }, connected: vec!(), edge_weights: vec!(), };
+                
+                nodes.push(cur_node);
 
-                matrix[i][j] = 1;
+                let res = attach_nodes(mtx, nodes, left_nodes, node_count);
+                nodes = res.0; left_nodes = res.1;
             }
         }
     }
@@ -165,50 +168,51 @@ fn graph_init(mtx: &[Vec<u8>], size: usize, start_x: usize, start_y: usize, end_
         parent_index: 0
     }, connected: vec!(), edge_weights: vec!() });
 
-    
-    //Connect the nodes
-    let l = nodes.len();
-    let mut left_nodes: Vec<usize> = vec![usize::MAX; size]; //Container to store the last node to the left of current with a given y value
-    for i in 0..l-1
-    {
-        let n1 = nodes[i].node;
-        //connect to node below if no wall is between
-        if nodes[i].node.x == nodes[i+1].node.x &&
-        !wall_between_nodes_vert(mtx, &nodes[i+1], &nodes[i])
-        {
-            let n2 = nodes[i+1].node;
-            let diff = (nodes[i].node.y as i32 - nodes[i+1].node.y as i32).abs() as u16;
-            nodes[i].connected.push(n2.index);
-            nodes[i+1].connected.push(n1.index);
-            nodes[i].edge_weights.push(diff);
-            nodes[i+1].edge_weights.push(diff);
-        }
+    node_count += 1;
+    let res = attach_nodes(mtx, nodes, left_nodes, node_count);
+    nodes = res.0;
 
-        //connect to node laterally 
-        if left_nodes[nodes[i].node.y] != usize::MAX &&
-        !wall_between_nodes_horz(mtx, &nodes[left_nodes[nodes[i].node.y]], &nodes[i])
-        {
-            let n2 = nodes[left_nodes[nodes[i].node.y]].node;
-            let diff = (nodes[i].node.x as i32 - n2.x as i32).abs() as u16;
-
-            nodes[i].connected.push(n2.index);
-            nodes[n2.index].connected.push(n1.index);
-
-            nodes[i].edge_weights.push(diff);
-            nodes[n2.index].edge_weights.push(diff);
-        }
-
-        left_nodes[nodes[i].node.y] = nodes[i].node.index;
-
-        
-    }
     println!("Node count: {}", node_count);
     println!("Size of graph: {}", size_of_val(&*nodes));
     nodes
 }
 
+fn attach_nodes(mtx: &[Vec<u8>], mut nodes: Vec<Node>, mut left_nodes: Vec<usize>, node_count: usize) -> (Vec<Node>, Vec<usize>)
+{
+    let n1 = nodes[node_count-1].node;
+    //connect to node below if no wall is between
+    if nodes[node_count-1].node.x == nodes[node_count].node.x &&
+    !wall_between_nodes_vert(mtx, &nodes[node_count], &nodes[node_count-1])
+    {
+        let n2 = nodes[node_count].node;
+        let diff = (nodes[node_count-1].node.y as i32 - nodes[node_count].node.y as i32).abs() as u16;
+        nodes[node_count-1].connected.push(n2.index);
+        nodes[node_count].connected.push(n1.index);
+        nodes[node_count-1].edge_weights.push(diff);
+        nodes[node_count].edge_weights.push(diff);
+    }
+
+    //connect to node laterally 
+    if left_nodes[nodes[node_count-1].node.y] != usize::MAX &&
+    !wall_between_nodes_horz(mtx, &nodes[left_nodes[nodes[node_count-1].node.y]], &nodes[node_count-1])
+    {
+        let n2 = nodes[left_nodes[nodes[node_count-1].node.y]].node;
+        let diff = (nodes[node_count-1].node.x as i32 - n2.x as i32).abs() as u16;
+
+        nodes[node_count-1].connected.push(n2.index);
+        nodes[n2.index].connected.push(n1.index);
+
+        nodes[node_count-1].edge_weights.push(diff);
+        nodes[n2.index].edge_weights.push(diff);
+    }
+
+    left_nodes[nodes[node_count-1].node.y] = nodes[node_count-1].node.index;  
+
+    (nodes, left_nodes)
+}
+
 //n1 is above n2 and shares the same x
-pub fn wall_between_nodes_vert(mtx: &[Vec<u8>], n1: &Node, n2: &Node) -> bool
+fn wall_between_nodes_vert(mtx: &[Vec<u8>], n1: &Node, n2: &Node) -> bool
 {
     let x = n1.node.x;
     for i in n2.node.y..n1.node.y
@@ -219,7 +223,7 @@ pub fn wall_between_nodes_vert(mtx: &[Vec<u8>], n1: &Node, n2: &Node) -> bool
 }
 
 //n1 is left of n2 and shares the same y
-pub fn wall_between_nodes_horz(mtx: &[Vec<u8>], n1: &Node, n2: &Node) -> bool
+fn wall_between_nodes_horz(mtx: &[Vec<u8>], n1: &Node, n2: &Node) -> bool
 {
     let y = n1.node.y;
     for i in n1.node.x..n2.node.x
